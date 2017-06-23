db62d9c92642c092a8c2bc1155836295098bf696
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2017-06-23 19:32:10 +0700

    add VS project

--- clean.bat
+++ clean.bat
@@ -4,13 +4,16 @@ del src\Makefile.*
 rd src\debug /S /Q 
 rd src\release /S /Q
 rd src\moc /S /Q
+rd src\x64 /S /Q
 
 del test\test_rx\Makefile.*
 rd test\test_rx\debug /S /Q 
 rd test\test_rx\release /S /Q
 rd test\test_rx\moc /S /Q
+rd test\test_rx\x64 /S /Q
 
 del test\test_tx\Makefile.*
 rd test\test_tx\debug /S /Q 
 rd test\test_tx\release /S /Q
 rd test\test_tx\moc /S /Q
+rd test\test_tx\x64 /S /Q
